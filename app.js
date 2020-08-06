7373c4646fcc613f9c998395abb3bb719122e821
Pedro <pedro.rafael@genialinvestimentos.com.br>
2020-08-05 22:52:52 -0300

    api para comunicação direta

--- app.js
+++ app.js
@@ -9,23 +9,19 @@ var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url);
 });
-server.get('/',(req, res, next) => { res.send('This server is a channel to LGPD - Bot')})
+server.use(restify.plugins.bodyParser({ mapParams: true }));
+server.get('/', (req, res, next) => { res.send('This server is a channel to LGPD - Bot') })
 
-telBot.start((msg) => {
-    watson.responseUser(msg.text || 'Desculpa, mas não tenho resposta para isso no momento', (response) => {
-        telBot.send(response)
-    })
+server.post('/watson', (req, res, next) => {
+    watson.responseUser(req.body.msg || 'Desculpa, mas não tenho resposta para isso no momento',
+        (response) => {
+            res.send(response)
+        })
 })
 
-
-
-// var json = require('./watson/skill-V2.json')
-// var helper = require('./watson/skillHelper')
-// var jsonData = JSON.stringify(helper.module.addDialogNode(json))
-
-// var fs = require('fs');
-// fs.writeFile("./watson/skill.json", jsonData, function(err) {
-//     if (err) {
-//         console.log(err);
-//     }
-// });
+// telBot.start((msg) => {
+//     watson.responseUser(msg.text || 'Desculpa, mas não tenho resposta para isso no momento',
+//         (response) => {
+//             telBot.send(response)
+//         })
+// })
